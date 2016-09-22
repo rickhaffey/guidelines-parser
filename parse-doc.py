111545a9270f931321db0eb230a68360f881caca
Rick Haffey <rhaffey@fanatics.com>
2016-09-22 09:45:01 -0500

    move section parsing into main script; reorg code

--- parse-doc.py
+++ parse-doc.py
@@ -1,17 +1,72 @@
 import docx
-import parse_sections
 import json
+import re
+import sys
+
+default_root = "/Users/rhaffey/Dropbox/Projects/EMS/pdf-work"
+
+json_category = "category"
+json_guidelines = "guidelines"
+json_guideline = "guideline"
+json_title = "title"
+json_sections = "sections"
+json_p_index = "p_index"
+json_heading = "heading"
+json_text = "text"
+json_indent = "indent"
+
+sectionRegexes = {
+  r"""secondary.*assessment.*treatment.*interventions""": "Secondary Assessment, Treatment, and Interventions",
+  r"""assessment.*treatment.*interventions""": "Assessment, Treatment, and Interventions",
+  r"""treatment.*interventions""": "Treatment and Interventions",
+  r"""assessment""": "Assessment",
+  r"""definitions""": "Definitions",
+  r"""inclusion.*exclusion.*criteria""": "Inclusion / Exclusion Criteria",
+  r"""exclusion.*criteria""": "Exclusion Criteria",
+  r"""inclusion.*criteria""": "Inclusion Criteria",
+  r"""key.*considerations""": "Key Considerations",
+  r"""key.*documentation.*elements""": "Key Documentation Elements",
+  r"""notes.*educational.*pearls""": "Notes / Educational Pearls",
+  r"""patient.*care.*goals""": "Patient Care Goals",
+  r"""patient.*management""": "Patient Management",
+  r"""patient.*presentation""": "Patient Presentation",
+  r"""patient.*safety.*considerations""": "Patient Safety Considerations",
+  r"""performance.*measures""": "Performance Measures",
+  r"""pertinent.*assessment.*findings""": "Pertinent Assessment Findings",
+  r"""quality.*improvement""": "Quality Improvement",
+  r"""references""": "References",
+  r"""special.*transport.*considerations""": "Special Transport Considerations",
+  r"""scene.*management""": "Scene Management"
+}
 
 def build_category(text):
-    return ("category", text.strip())
+    return (json_category, text.strip())
+
 
 def build_guideline(text, i, category):
-    return ("guideline", {
-        "title": text.strip(),
-        "category": category,
-        "sections": [],
-        "idx": i
+    return (json_guideline, {
+        json_title: text.strip(),
+        json_category: category,
+        json_sections: [],
+        json_p_index: i
     })
+
+
+def build_section(text, i):
+    return {
+        json_heading: text,
+        json_text: [],
+        json_p_index: i
+    }
+
+
+def build_section_text(text, i, indent):
+    return {
+        json_text: text,
+        json_indent: indent,
+        json_p_index: i
+    }
+
 
 def parse_paragraph(p, i, category = None):
     style = p.style.name
@@ -28,7 +83,24 @@ def parse_paragraph(p, i, category = None):
             runheading = runheading + r.text
 
     if(runheading.strip()):
-        return build_guideline(runheading, category)
+        return build_guideline(runheading, i, category)
+
+    return None
+
+
+def parse_section_header(p):
+    rtext = ""
+    isSection = False
+    for r in p.runs:
+        rtext = rtext + r.text.strip()
+        f = r.font
+        isSection = isSection or (f.bold and f.underline)
+
+    if(isSection):
+        # check to see whether it matches one of our regexes
+        for regex in sectionRegexes.keys():
+            if(re.match(regex, rtext, re.I)):
+                return sectionRegexes[regex]
 
     return None
 
@@ -37,56 +109,83 @@ def get_ppr(p):
   """Gets the product properties (serialized as <pPr> in the .docx xml) for the paragraph"""
   return p._element.find("{http://schemas.openxmlformats.org/wordprocessingml/2006/main}pPr")
 
-def get_ilvl(ppr):
+
+def get_ilvl(p):
   """Gets the indent level (serialized as <pPr><numPr><ilvl val="_"> in the .docx xml) from the product properties"""
+  ppr = get_ppr(p)
+
   if(ppr is None or ppr.numPr is None or ppr.numPr.ilvl is None or ppr.numPr.ilvl.val is None):
     return 0
   return int(ppr.numPr.ilvl.val) + 1
 
 
+def show_progress(marker):
+    print(marker, end='', flush=True)
+
+
+def get_infile_path():
+    if(len(sys.argv) >= 2):
+        return sys.argv[1]
+    else:
+        return default_root + "/guidelines.docx"
+        #return default_root + "/guidelines.partial.docx"
+
+
+def get_outfile_path():    
+    if(len(sys.argv) >= 3):
+        return sys.argv[2]
+    else:
+        return default_root + "/output/out.json"
+
+    
 def main():
-#    path = "/Users/rhaffey/Dropbox/Projects/EMS/pdf-work/guidelines.docx"
-    path = "/Users/rhaffey/Dropbox/Projects/EMS/pdf-work/guidelines.partial.docx"
+    # read in the input word doc
+    path = get_infile_path()
     source_doc = docx.Document(path)
 
-    start = 0
-    p_count = len(source_doc.paragraphs)
-
+    # build the starting point of an output doc
     out_doc = {
-        "guidelines": []
+        json_guidelines: []
     }
 
-    category = None
-    guideline = None
+    # build some preliminary 'running' objects
+    running_category = None
+    running_guideline = None
     running_section = None
-    for i in range(start, start + p_count):
+
+    # iterate over the paragraphs in the doc
+    start = 0
+    end = start + len(source_doc.paragraphs)
+    for i in range(start, end):
         p = source_doc.paragraphs[i]
 
-        parsed = parse_paragraph(p, i, category)
+        # first, handle new category and guideline paragraphs
+        parsed = parse_paragraph(p, i, running_category)
         if(parsed):
             if(parsed[0] == "category"):
-                category = parsed[1]
-                print('#', end='', flush=True)
+                running_category = parsed[1]
+                show_progress("#")
             elif(parsed[0] == "guideline"):
-                guideline = parsed[1]
-                out_doc["guidelines"].append(guideline)
-                print('+', end='', flush=True)
+                running_guideline = parsed[1]
+                out_doc[json_guidelines].append(running_guideline)
+                show_progress("+")
 
-        section = parse_sections.get_section_header(p)
+        # next, parse out any section details (headers, and section text)
+        section = parse_section_header(p)
         if(section):
-            running_section = { "heading": section, "text": [], "idx": i }
-            guideline["sections"].append(running_section)
+            running_section = build_section(section, i)
+            running_guideline[json_sections].append(running_section)
         elif(parsed == None and running_section != None):
             outText = p.text.strip()
             if(outText != ""):
-                print(".", end="", flush=True)
-                ppr = get_ppr(p)
-                ilvl = get_ilvl(ppr)
-                running_section["text"].append({ "text": p.text, "lvl": ilvl, "idx": i })
+                section_text = build_section_text(p.text, i, get_ilvl(p))
+                running_section["text"].append(section_text)
+                show_progress(".")
             else:
-                print("x", end="", flush=True)
+                show_progress("x")
 
-    with open('output/out.indented.json', 'w') as outfile:
+    # write the final document in .json format to the outfile
+    with open(get_outfile_path(), 'w') as outfile:
         outfile.write(json.dumps(out_doc, indent=2))
 
 
