3318b6636ef5fabad44b8fa323cfb11a6a9e5ca3
Rick Haffey <rhaffey@fanatics.com>
2016-09-23 08:13:05 -0500

    add hierarchical structure

--- parse-doc.py
+++ parse-doc.py
@@ -13,32 +13,58 @@ json_sections = "sections"
 json_p_index = "p_index"
 json_heading = "heading"
 json_text = "text"
+json_items = "items"
 json_indent = "indent"
 json_nemsis = "nemsis_ref"
 
+# json section names
+json_s_defintions = "defintions"
+json_s_patientCareGoals = "patientCareGoals"
+json_s_patientPresentation = "patientPresentation"
+json_s_inclusionCriteria = "inclusionCriteria"
+json_s_exclusionCriteria = "exclusionCriteria"
+json_s_inclusionExclusionCriteria = "inclusionExclusionCriteria"
+json_s_specialTransporConsiderations = "specialTransporConsiderations"
+json_s_sceneManagement = "sceneManagement"
+json_s_patientManagement = "patientManagement"
+json_s_assessment = "assessment"
+json_s_treatmentAndInterventions = "treatmentAndInterventions"
+json_s_patientSafetyConsiderations = "patientSafetyConsiderations"
+json_s_secondaryAssesmentTreatmentAndInterventions = "secondaryAssesmentTreatmentAndInterventions"
+json_s_assessmentTreatmentAndInterventions = "assessmentTreatmentAndInterventions"
+json_s_notesAndEducationalPearls = "notesAndEducationalPearls"
+json_s_keyConsiderations = "keyConsiderations"
+json_s_pertinentAssessmentFindings = "pertinentAssessmentFindings"
+json_s_qualityImprovement = "qualityImprovement"
+json_s_keyDocumentationElements = "keyDocumentationElements"
+json_s_performanceMeasures = "performanceMeasures"
+json_s_references = "references"
+
+
 sectionRegexes = {
-  r"""secondary.*assessment.*treatment.*interventions""": "Secondary Assessment, Treatment, and Interventions",
-  r"""assessment.*treatment.*interventions""": "Assessment, Treatment, and Interventions",
-  r"""treatment.*interventions""": "Treatment and Interventions",
-  r"""assessment""": "Assessment",
-  r"""definitions""": "Definitions",
-  r"""inclusion.*exclusion.*criteria""": "Inclusion / Exclusion Criteria",
-  r"""exclusion.*criteria""": "Exclusion Criteria",
-  r"""inclusion.*criteria""": "Inclusion Criteria",
-  r"""key.*considerations""": "Key Considerations",
-  r"""key.*documentation.*elements""": "Key Documentation Elements",
-  r"""notes.*educational.*pearls""": "Notes / Educational Pearls",
-  r"""patient.*care.*goals""": "Patient Care Goals",
-  r"""patient.*management""": "Patient Management",
-  r"""patient.*presentation""": "Patient Presentation",
-  r"""patient.*safety.*considerations""": "Patient Safety Considerations",
-  r"""performance.*measures""": "Performance Measures",
-  r"""pertinent.*assessment.*findings""": "Pertinent Assessment Findings",
-  r"""quality.*improvement""": "Quality Improvement",
-  r"""references""": "References",
-  r"""special.*transport.*considerations""": "Special Transport Considerations",
-  r"""scene.*management""": "Scene Management"
+  r"""secondary.*assessment.*treatment.*interventions""": [json_s_patientManagement, json_s_secondaryAssesmentTreatmentAndInterventions],
+  r"""assessment.*treatment.*interventions""": [json_s_patientManagement, json_s_assessmentTreatmentAndInterventions],
+  r"""treatment.*interventions""": [json_s_patientManagement, json_s_treatmentAndInterventions],
+  r"""assessment""": [json_s_patientManagement, json_s_assessment],
+  r"""definitions""": [json_s_defintions],
+  r"""inclusion.*exclusion.*criteria""": [json_s_patientPresentation, json_s_inclusionExclusionCriteria],
+  r"""exclusion.*criteria""": [json_s_patientPresentation, json_s_exclusionCriteria],
+  r"""inclusion.*criteria""": [json_s_patientPresentation, json_s_inclusionCriteria],
+  r"""key.*considerations""": [json_s_notesAndEducationalPearls, json_s_keyConsiderations],
+  r"""key.*documentation.*elements""": [json_s_qualityImprovement, json_s_keyDocumentationElements],
+  r"""notes.*educational.*pearls""": [json_s_notesAndEducationalPearls],
+  r"""patient.*care.*goals""": [json_s_patientCareGoals],
+  r"""patient.*management""": [json_s_patientManagement],
+  r"""patient.*presentation""": [json_s_patientPresentation],
+  r"""patient.*safety.*considerations""": [json_s_patientManagement, json_s_patientSafetyConsiderations],
+  r"""performance.*measures""": [json_s_qualityImprovement, json_s_performanceMeasures],
+  r"""pertinent.*assessment.*findings""": [json_s_notesAndEducationalPearls, json_s_pertinentAssessmentFindings],
+  r"""quality.*improvement""": [json_s_qualityImprovement],
+  r"""references""": [json_s_references],
+  r"""special.*transport.*considerations""": [json_s_specialTransporConsiderations],
+  r"""scene.*management""": [json_s_sceneManagement]
 }
+
 
 def build_category(text):
     return (json_category, text.strip())
@@ -48,7 +74,7 @@ def build_guideline(text, i, category):
     return (json_guideline, {
         json_title: text.strip(),
         json_category: category,
-        json_sections: [],
+        # sections - will be added dynamically as they're found in the source doc
         json_nemsis: None,
         json_p_index: i
     })
@@ -60,11 +86,10 @@ def build_nemsis_ref(text, i):
     })
 
 
-def build_section(text, i):
+def build_section(i):
     return {
-        json_heading: text,
-        json_text: [],
-        json_p_index: i
+        json_p_index: i,
+        json_items: []
     }
 
 
@@ -154,14 +179,14 @@ def get_infile_path():
         #return default_root + "/guidelines.partial.docx"
 
 
-def get_outfile_path():    
+def get_outfile_path():
     if(len(sys.argv) >= 3):
         return sys.argv[2]
     else:
-        #return default_root + "/output/out.json"
-        return default_root + "/output/out.partial.json"
+        return default_root + "/output/out.json"
+        #return default_root + "/output/out.partial.json"
 
-    
+
 def main():
     # read in the input word doc
     path = get_infile_path()
@@ -191,25 +216,34 @@ def main():
         if(parsed):
             if(parsed[0] == json_category):
                 running_category = parsed[1]
-                show_progress("#")
+                show_progress("\n# " + running_category)
             elif(parsed[0] == json_guideline):
                 running_guideline = parsed[1]
                 out_doc[json_guidelines].append(running_guideline)
-                show_progress("+")
+                show_progress("\n\t+")
             elif(parsed[0] == json_nemsis):
                 running_guideline[json_nemsis] = parsed[1]
                 show_progress("n")
 
         # next, parse out any section details (headers, and section text)
-        section = parse_section_header(p)
-        if(section):
-            running_section = build_section(section, i)
-            running_guideline[json_sections].append(running_section)
+        section_path = parse_section_header(p)
+        if(section_path):
+            # iterate through the elements of the path, creating any if they're not found
+            parent = running_guideline
+            for section_path_element in section_path:
+                element = parent.get(section_path_element)
+                if(element is None):
+                    element = build_section(i)
+                    parent[section_path_element] = element
+
+                parent = element
+
+            running_section = element
         elif(parsed == None and running_section != None):
             outText = p.text.strip()
             if(outText != ""):
                 section_text = build_section_text(p.text, i, get_ilvl(p))
-                running_section[json_text].append(section_text)
+                running_section[json_items].append(section_text)
                 show_progress(".")
             else:
                 show_progress("x")
